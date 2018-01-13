0e1a57b4e0c61de232cec3943b1633949c6d2c64
Sebitta <34277607+Sebitta@users.noreply.github.com>
2018-01-13 15:37:41 -0300

    prueba nueva

--- ui/frmservicios.php
+++ ui/frmservicios.php
@@ -54,7 +54,7 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
         <!--keypress-->
         <script>
-            function buscar_nombre() //pruebaSeba
+            function buscar_nombre() //prueba
             {
                 data = '';
                 marco = 'marco_nombres';
