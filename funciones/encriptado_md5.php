b88fb96ca4371b7cc13c0ad5c466ac39dc07e2d1
Sebitta <34277607+Sebitta@users.noreply.github.com>
2018-01-26 15:41:42 -0300

    add archivo para encriptar las pass

---
+++ funciones/encriptado_md5.php
@@ -0,0 +1,25 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <script type="text/javascript" src="code.js"></script>
+        <script>
+        function encriptar(elemento){
+            var palabra = document.getElementsByTagName("input")[0].value;
+            var palabra_codificada = btoa(palabra);
+            document.getElementById("resultado").innerHTML = palabra_codificada;
+        }
+        function desencriptar(elemento){
+            var palabra = document.getElementsByTagName("input")[0].value;
+            var palabra_decodificada = atob(palabra);
+            document.getElementById("resultado").innerHTML = palabra_decodificada;
+        }
+        </script>
+    </head>
+    <body>
+        <input type="password" id="caja">
+        <button onclick="encriptar(this)">encriptar</button>
+        <button onclick="desencriptar(this)">desencriptar</button>
+        <p id="resultado"></p>
+    </body>
