e75359fba4b5aaed0b4de0803f4682db5aad4005
Alexis <alexisjmp@gmail.com>
2018-01-30 11:30:20 -0300

    prueba

---
+++ funciones/listservicios_1.php
@@ -0,0 +1,54 @@
+<?php
+
+//require_once('../clases/agenda.php');
+//require_once('../clases/paciente.php');
+//require_once('../servicio/funciones_adicionales.php');
+//require_once('../clases/cls_servicio.php');
+require_once('../negocio/reglasdatos.php');
+ 
+
+
+listarconfirmados();
+
+function listarconfirmados(){
+    // $nodo = new cls_servicio();
+   //  unset($nodo->arrservicio);
+   //  $nodo->select();
+     $arrservicio= listaservicios()
+  	?>
+                        
+     <label> : </label> <label id="">  
+      <input id="blockeo_agenda" value="1" hidden>
+                                   
+                         
+      <table class="table table-condensed table-bordered" id="tableconfirma">
+        <thead>
+            <tr class="traltura">
+            <th>N</th>
+            <th>id</th>
+            <th>Nombre</th>
+            <th>Categoria</th>
+            
+             </tr>
+        </thead>
+     <tbody class="table-body">             
+            <?php
+            
+            for($i=0;$i< count($arrservicio);$i++){ ;
+             echo $i;
+	?>
+ <tr  class="traltura">
+     <td><?php echo ($i+1);?></td>
+ 
+    <td><?php echo $arrservicio[$i]['id_nombre'] ;?></td>
+    <td><?php echo $arrservicio[$i]['nombre_servicio'] ;?></td>
+     <td><?php echo $arrservicio[$i]['nombre_categoria'] ;?></td>
+ </tr>
+  
+ <?php }	?>
+              
+</tbody>
+         </table>
+                            
+   	<?php } ?>
+
