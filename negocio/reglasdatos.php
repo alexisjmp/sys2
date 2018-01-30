4c16e3f9a8e2f90c8a2133e42cb8fbfd94bb685c
Alexis <alexisjmp@gmail.com>
2018-01-30 11:30:58 -0300

    capa de reglas de negocio

---
+++ negocio/reglasdatos.php
@@ -0,0 +1,96 @@
+<?php
+require_once('../clases/cls_servicio.php');
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+if($_POST['funcion']== 'listaservicios')
+   listaservicios(); 
+if($_POST['funcion']== 'buscaservicios')
+   buscaservicios();
+if($_POST['funcion']== 'munestadoservicio')
+   munestadoservicio();
+if($_POST['munservicio']== 'munservicio')
+   munservicio();
+if($_POST['funcion']== 'miiservicio')
+   miiservicio();
+if($_POST['munservicio']== 'dnneleminaservicio')
+   dnneleminaservicio();
+// **********************
+// SERVICIO
+// **********************
+function msolistaservicios(){
+     $nodo = new cls_servicio();
+     unset($nodo->arrservicio);
+     $nodo->select();
+     //$myJSON = json_encode($nodo->arrservicio);
+     //echo $myJSON;
+     return $nodo->arrservicio;
+   
+}
+function msobuscaservicios($opc, $nombre_servicio, $id_categoria){
+     $nodo = new cls_servicio();
+     $nodo->setopc($opc);
+     $nodo->setnombre_servicio($nombre_servicio);
+     $nodo->setid_categoria($id_categoria);
+     $nodo->busquedaservicios();
+     //$myJSON = json_encode($nodo->arrservicio);
+     //echo $myJSON;
+     return $nodo->arrservicio;
+   
+}
+function munestadoservicio(){
+    $id_servicio = $_POST['id_servicio'];
+    $estado_ervicio = $_POST['estado_servicio'];
+    $nodo = new cls_servicio();
+    $nodo->setid_servicio($id_servicio);
+    $nodo->setestado_servicio($estado_servicio);
+    $nodo->updateestado();
+}
+function munservicio(){
+    $id_servicio = $_POST['id_servicio'];
+    $id_categoria = $_POST['id_categoria'];
+    $nombre_servicio = $_POST['_servicio'];
+    $descripcion_servicio = $_POST['_servicio'];
+    //$estado_servicio = $_POST['estado_servicio'];
+    $precio = $_POST['precio'];
+        
+    $nodo = new cls_servicio();
+    $nodo->setid_servicio($id_servicio);
+    $nodo->setid_categoria($id_categoria);
+    $nodo->setnombre_servicio($nombre_servicio);
+    $nodo->setdescripcion_servicio($descripcion_servicio);
+    //$nodo->setestado_servicio($estado_servicio);
+    $nodo->setprecio($precio);
+    $nodo->update();
+}
+function miiservicio(){
+    $id_servicio = $_POST['id_servicio'];
+    $id_categoria = $_POST['id_categoria'];
+    $nombre_servicio = $_POST['_servicio'];
+    $descripcion_servicio = $_POST['_servicio'];
+    //$estado_servicio = $_POST['estado_servicio'];
+    $precio = $_POST['precio'];
+        
+    $nodo = new cls_servicio();
+    $nodo->setid_servicio($id_servicio);
+    $nodo->setid_categoria($id_categoria);
+    $nodo->setnombre_servicio($nombre_servicio);
+    $nodo->setdescripcion_servicio($descripcion_servicio);
+    //$nodo->setestado_servicio($estado_servicio);
+    $nodo->setprecio($precio);
+    $nodo->insert();
+}
+function mdneleminaservicio(){
+    $id_servicio = $_POST['id_servicio'];
+    //$estado_ervicio = $_POST['estado_servicio'];
+    $nodo = new cls_servicio();
+    $nodo->setid_servicio($id_servicio);
+    //$nodo->setestado_servicio($estado_servicio);
+    $nodo->delete();
+}
+// **********************
+// Categoria
+// **********************
+?>
