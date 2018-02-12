a68b2d01933f7fd9910eacd0f1fd65d9567a9ea6
Alexis <alexisjmp@gmail.com>
2018-02-11 23:52:45 -0300

    ajuste en la ui

--- reporte/pdf_cotizacion.php
+++ reporte/pdf_cotizacion.php
@@ -7,14 +7,14 @@ require('../clases/fpdf181/fpdf.php');
 
 require_once('../clases/cls_pdf.php');
 
-
+$total_servicio =$_GET['total_servicio'];
 $myJSON = json_decode($_GET['myJSON1']);
 
-report($myJSON);
+report($myJSON,$total_servicio);
 
 
 
-function report($myJSON) {
+function report($myJSON,$total_servicio) {
 // Instanciation of inherited class
     $pdf = new PDF('L', 'mm', 'Letter');
     $pdf->AliasNbPages();
@@ -24,17 +24,18 @@ function report($myJSON) {
 // $fecha = '2017-11-20';
 
 
-    $pdf->FECHATitle('', 'FECHA:' . date('Y-m-d h:i '));
+    $pdf->FECHATitle('', 'FECHA:' . date('d/m/Y H:i '));
     $pdf->Cell(0, 4, '', 0, 1);
 
     //$pdf->ChapterTitle2(' Reporte agenda - paciente: '.getnombrepaciente($id_paciente),' ');
 // $fecha = cambiaf_a_mysql($_POST['fecha_atencion_in']);
 
-    imprimirrpt($myJSON, $pdf);
+    imprimirrpt($myJSON, $pdf,$total_servicio);
     $pdf->Output();
 }
 
-function imprimirrpt($arr, $pdf) {
+function imprimirrpt($arr, $pdf,$total_servicio) {
+$pdf->FECHATitle('','Valor Servicio Total:'.$total_servicio );    
 $w = array(60, 45, 60, 45,45);
         for ($i = 0; $i < count($arr); $i++) {
             // hora-tipo atencion, nombre-apellido-fecha nacimiento-estado civil -estado-fono-fono alternativo   
