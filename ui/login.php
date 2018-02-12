8741300b74eeb6085c8d8a6137fa672467eb9b59
DiegoHuanaco <Diego.huan.ram@gmail.com>
2018-02-12 16:34:22 -0300

    ajax login

---
+++ ui/login.php
@@ -0,0 +1,33 @@
+<?php 
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+     ?>                       <h2></h2>
+
+                            <div class="form-group">
+                                <label for="rut"><h4>RUT</h4></label>
+                                <select class="form-control" id="rut">
+                                    <option value=""></option 
+                                    <option value="0">11111</option>
+                                     <option value="1">11111</option>
+
+                                    <option value="6"></option>
+
+
+
+
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="pwd"><h4>PASSWORD</h4></label>
+                                <input type="password" class="form-control" id="pwd">
+                            </div>
+                            <div class="checkbox">
+                                <label><input type="checkbox">Recuerdeme</label>
+                            </div>
+                            <button type="button"  class="btn btn-info">Entrar</button><!-- fin contenido tabla modal-->
+                            <button type="button" onclick="registro();" class="btn btn-info">Registrarse</button><!-- fin contenido tabla modal-->
+
