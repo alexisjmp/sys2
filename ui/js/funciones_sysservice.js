9f89b3496d8a0b331f522daf140583cea56743bc
DiegoHuanaco <Diego.huan.ram@gmail.com>
2018-02-12 16:35:02 -0300

    nuevo ajax

--- ui/js/funciones_sysservice.js
+++ ui/js/funciones_sysservice.js
@@ -19,3 +19,20 @@ function AbrirVentana(pagina, parametros, nombre, pancho, palto){
 	walto = (screen.height - palto)/2;
 	window.open(pagina+"?"+parametros,nombre,"height="+palto+",width="+pancho+",top="+walto+",left="+wancho+",toolbar=0,location=0,directories=0,status=0,menubar=0,scrollbars=1");
 }
+
+
+  function sendajax_modal(marco,ruta,data){
+      
+     var xhttp = new XMLHttpRequest();
+     xhttp.onreadystatechange = function() {
+     if (this.readyState == 4 && this.status == 200) {
+       
+      
+            document.getElementById(marco).innerHTML = "";  
+            document.getElementById(marco).innerHTML = this.responseText;
+        }
+     };
+     xhttp.open("POST",ruta , true);
+     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+     xhttp.send(data); 
+  }
