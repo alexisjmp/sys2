413280148053766f0bce96f20f044115f2121b09
DiegoHuanaco <Diego.huan.ram@gmail.com>
2018-02-14 17:05:37 -0300

    nuevo archivo

---
+++ ui/login_1.php
@@ -0,0 +1,32 @@
+<?php 
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+     ?>                       <h2></h2>
+
+                            <div class="form-group">
+                                <label for="rut"><h4>RUT</h4></label>
+                                <select class="form-control" id="rut_login_2">
+                                    <option value=""></option 
+                                    <option value="0">11111</option>
+                                     <option value="1">11111</option>
+
+                                    <option value="6"></option>
+
+
+
+
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="pwd"><h4>PASSWORD</h4></label>
+                                <input type="password" class="form-control" id="pwd_login_2">
+                            </div>
+                            <div class="checkbox">
+                                <label><input type="checkbox">Recuerdeme</label>
+                            </div>
+     <button type="button"  onclick="entrar_2()" class="btn btn-info">Entrar</button><!-- fin contenido tabla modal-->
+
